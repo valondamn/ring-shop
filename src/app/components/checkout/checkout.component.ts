37111d1d2d512e35c1674110e7381a0a2ff68a33
aruym <190103062@stu.sdu.edu.kz>
2024-04-18 20:59:39 +0500

    changed cart

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -4,7 +4,7 @@ import {CartModelServer} from "../../models/cart.model";
 import {Router} from "@angular/router";
 import {OrderService} from "../../services/order.service";
 import {NgxSpinnerService} from "ngx-spinner";
-import {FormBuilder, NgForm, Validators} from "@angular/forms";
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'mg-checkout',
@@ -41,7 +41,7 @@ export class CheckoutComponent implements OnInit {
 
   onCheckout() {
    this.spinner.show().then(p => {
-      this.cartService.CheckoutFromCart(1);
+      this.cartService.CheckoutFromCart(2);
     });
 
 
